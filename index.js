93acdc39fe241d6587ee1c903f476286c093a839
shwibi <shwibishrabs285@gmail.com>
2021-07-01 11:50:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * shwijs-physics
+ * Copyright @Shwi 2021
+ */
+
+const Angle = require("./Angle");
+const PhysicsBody = require("./PhysicsBody");
+const Vector = require("./Vector");
+
+// Exports
+module.exports = {
+	Angle,
+	PhysicsBody,
+	Vector,
+};
